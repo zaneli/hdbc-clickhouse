46324ca4fe5db0ec7d3a1e67242b89aee4ee1dfd
zaneli <shun.otani@gmail.com>
2020-07-25 15:37:00 +0900

    fix `columnTypeName` to be exhaustive all types

--- src/Database/HDBC/ClickHouse/Data/Column.hs
+++ src/Database/HDBC/ClickHouse/Data/Column.hs
@@ -44,5 +44,22 @@ data Column = StringColumn {
 } deriving Show
 
 columnTypeName :: Column -> String
-columnTypeName (StringColumn _) = "String"
--- TODO: fix all types
+columnTypeName (StringColumn _)           = "String"
+columnTypeName (Int8Column _)             = "Int8"
+columnTypeName (Int16Column _)            = "Int16"
+columnTypeName (Int32Column _)            = "Int32"
+columnTypeName (Int64Column _)            = "Int64"
+columnTypeName (UInt8Column _)            = "UInt8"
+columnTypeName (UInt16Column _)           = "UInt16"
+columnTypeName (UInt32Column _)           = "UInt32"
+columnTypeName (UInt64Column _)           = "UInt64"
+columnTypeName (Float32Column _)          = "Float32"
+columnTypeName (Float64Column _)          = "Float64"
+columnTypeName (DateColumn _)             = "Date"
+columnTypeName (DateTimeColumn _)         = "DateTime"
+columnTypeName (UUIDColumn _)             = "UUID"
+columnTypeName (IPv4Column _)             = "IPv4"
+columnTypeName (IPv6Column _)             = "IPv6"
+columnTypeName (FixedStringColumn _ size) = "FixedString(" ++ (show size) ++ ")"
+columnTypeName (ArrayColumn _ item)       = "Array(" ++ (columnTypeName item) ++ ")"
+columnTypeName (NullableColumn _ item)    = "Nullable(" ++ (columnTypeName item) ++ ")"
